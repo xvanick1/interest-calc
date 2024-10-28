5e012be8fbcf0e5c4aea2d31d6b77c7fec06fffa
Jozef Vanický <506486@muni.cz>
2024-10-28 20:38:14 +0200

    feat: initial prototype implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,49 @@
-import Image from "next/image";
+"use client"
+
 import styles from "./page.module.css";
+import {useState} from "react";
 
 export default function Home() {
+  let [finalWealth, setFinalWealth] = useState(null);
+  let [deposit, setDeposit] = useState('');
+  let [apy, setApy] = useState('');
+  let [compound_freq_days, setCompound_freq_days] = useState('');
+  let [deposit_duration_days, setDeposit_duration_days] = useState('');
+
+  const handleValueChange = (event) => {
+    event.target.id === "deposit" ? setDeposit(event.target.value) : '';
+    event.target.id === "apy" ? setApy(event.target.value) : '';
+    event.target.id === "compound_freq_days" ? setCompound_freq_days(event.target.value) : '';
+    event.target.id === "deposit_duration_days" ? setDeposit_duration_days(event.target.value) : '';
+
+    calculateResult();
+  }
+
+  const calculateResult = () => {
+    if(!(deposit === '' || apy === '' || compound_freq_days === '' || compound_freq_days === '')) {
+       setFinalWealth(deposit * (1 + (apy / 100) / compound_freq_days) ** (compound_freq_days * deposit_duration_days))
+    }
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <h1>Let's get rich!</h1>
+        <div id="container">
+          <div id="inputForm">
+            <input id="deposit" type="string" placeholder="Initial Deposit Value" value={deposit} onChange={handleValueChange}/>
+            <input id="apy" type="string" placeholder="Yearly Interest Rate (APY)" value={apy} onChange={handleValueChange}/>
+            <input id="compound_freq_days" type="string" placeholder="How often in year is interest paid" value={compound_freq_days} onChange={handleValueChange}/>
+            <input id="deposit_duration_days" type="string" placeholder="Duration of deposit" value={deposit_duration_days} onChange={handleValueChange}/>
+            <span id="result">{finalWealth !== null ? finalWealth : "amount"}</span>
+          </div>
+          <div id="graph">
+            {/*  TODO: Graph*/}
+          </div>
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        Created by J.
       </footer>
     </div>
   );
