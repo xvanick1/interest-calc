9568e3e2b2f658a32d5d2ae4c0ac0f6e68cb031e
Jozef Vanický <506486@muni.cz>
2024-12-23 01:10:22 +0100

    build: prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: 'export', // Important for GitHub Pages
+	assetPrefix: '/interest-calc/', // Set the asset prefix
+	basePath: '/interest-calc', // Set the base path (optional)
+	images: {
+		unoptimized: true, // Often needed for GitHub Pages
+	},
+};
 
 export default nextConfig;
